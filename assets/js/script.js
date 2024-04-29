b872ef74bcfc636260aa87e4dbbb621a1ecc49fc
quintasmatheus <matheusleao.vp@gmail.com>
2024-04-29 22:53:28 +0100

    adding reset submit logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,3 +16,35 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
+
+//onsubmit="return handleSubmit();
+function handleSubmit() {
+  var form = document.getElementById('form');
+
+  fetch(form.action, {
+      method: form.method,
+      body: new FormData(form),
+      headers: {
+          'Accept': 'application/json',
+      },
+  })
+  .then(response => {
+      if (response.ok) {
+          form.reset(); 
+          alert('Form submetido com sucesso');
+      } else {
+          alert('O form não foi submetido com sucesso');
+      }
+  })
+  .catch(error => {
+      alert('Error submitting form');
+      console.error('Error:', error);
+  });
+
+  return false; 
+}
+
+//onsubmit="return validateForm();"
+function validateForm() {
+  return true; 
+}
